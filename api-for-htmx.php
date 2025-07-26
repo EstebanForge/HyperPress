915a521530a93dd9b200295d86c8d92649c5fb6f
Esteban <esteban@attitude.cl>
2025-07-25 21:22:30 -0400

    Styling

--- api-for-htmx.php
+++ api-for-htmx.php
@@ -4,7 +4,7 @@
  * Plugin Name: Hypermedia API for WordPress
  * Plugin URI: https://github.com/EstebanForge/Hypermedia-API-WordPress
  * Description: Adds API endpoints and integration for hypermedia libraries like HTMX, AlpineJS, and Datastar.
- * Version: 2.0.5
+ * Version: 2.1.0
  * Author: Esteban Cuevas
  * Author URI: https://actitud.xyz
  * License: GPLv2 or later
@@ -13,7 +13,7 @@
  * Domain Path: /languages
  * Requires at least: 6.4
  * Tested up to: 6.9
- * Requires PHP: 8.2.
+ * Requires PHP: 8.2
  */
 
 // Exit if accessed directly.
