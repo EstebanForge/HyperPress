3e5eea6b9f5602d34f4ae58e46cea32dd267367b
Esteban <esteban@attitude.cl>
2025-08-31 21:04:52 -0400

    cleanup

--- tests/helpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// Place shared test helpers here.
-function hm_fixture_path(string $relative): string {
-    return __DIR__ . '/_fixtures/' . ltrim($relative, '/');
-}
