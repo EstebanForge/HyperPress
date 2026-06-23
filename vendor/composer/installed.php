6878cad9cb77bd5ce97fe78c29dfa9aaf640bc0e
EstebanForge <1696145+EstebanForge@users.noreply.github.com>
2026-06-23 16:23:24 -0400

    Bump hyperpress version to 3.3.1
    Update estebanforge/hyperpress version metadata from 3.3.0 to 3.3.1. This updates composer.lock content-hash and vendor/composer/installed.php pretty_version/version entries to reflect the new release.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'estebanforge/hyperpress',
-        'pretty_version' => '3.3.0',
-        'version' => '3.3.0.0',
+        'pretty_version' => '3.3.1',
+        'version' => '3.3.1.0',
         'reference' => null,
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -47,8 +47,8 @@
             'dev_requirement' => false,
         ),
         'estebanforge/hyperpress' => array(
-            'pretty_version' => '3.3.0',
-            'version' => '3.3.0.0',
+            'pretty_version' => '3.3.1',
+            'version' => '3.3.1.0',
             'reference' => null,
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
